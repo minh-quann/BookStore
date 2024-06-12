cb22848bfb99353c9d3a11edcd227d7437f4f25e
minh-quann <quannguyen19092003@gmail.com>
2024-06-13 03:45:04 +0700

    Commit lan 1

---
+++ src/main/java/com/bookstore/repository/IUserRepository.java
@@ -0,0 +1,24 @@
+package com.bookstore.repository;
+
+import com.bookstore.entity.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface IUserRepository extends CrudRepository<User, Integer> {
+    @Query("SELECT u FROM User u WHERE u.username = ?1")
+    User findByUsername(String username);
+//    @Query("SELECT u FROM User u WHERE u.id = ?1")
+//    User findByUserId(Long userid);
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO user_role (user_id, role_id) VALUE (?1, ?2)", nativeQuery = true)
+    void addUserRole(Long userId, Long roleId);
+
+    @Query("SELECT u.id FROM User u WHERE u.username = ?1")
+    Long getUserIdByUsername(String username);
+
+    @Query(value = "SELECT r.name FROM role r INNER JOIN user_role ur ON r.id = ur.role_id WHERE ur.user_id = ?1", nativeQuery = true)
+    String[] getRoleOfUser(Long userId);
+}
